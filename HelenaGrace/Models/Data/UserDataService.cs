73c77c541c42d000e66f6ebf2ea6917651dfd643
Adam Bender <abend07@gmail.com>
2021-11-18 11:28:33 -0700

    Add project files.

---
+++ HelenaGrace/Models/Data/UserDataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelenaGrace.Models.Data
+{
+    public class UserDataService
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HelenaGrace;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool findUserByEmailAndPassword(User user)
+        {
+            bool success = false;
+
+            string sqlStatement = "SELECT * FROM dbo.USER WHERE EMAIL = @email and PASSWORD = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@email", System.Data.SqlDbType.VarChar, 40).Value = user.Email;
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                        success = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return success;
+        }
+    }
+}
---
+++ HelenaGrace/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelenaGrace.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Bio { get; set; }
+
+        public User(string firstName, string lastName, string email, string password, string phoneNumber, string bio)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Bio = bio;
+        }
+    }
+}
